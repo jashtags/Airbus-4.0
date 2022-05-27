2669a0e0ab07f008e600388f4d110e070b90f50f
hi <hi@gmail.com>
2022-05-27 12:48:15 +0530

    automation changes fro djongo check

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 import os
-os.system('pip install django')
+import sys
+import fileinput
+
+cwd = os.getcwd()
+path="/Users/jashtanukonda/firstdjangoproj/firstdjangoproj/"
+os.chdir(path)
+
+with open('settings.py', 'r') as file :
+    filedata = file.read()
+filedata = filedata.replace('djongo', 'changed ')
+with open('settings.py', 'w') as file:
+    file.write(filedata)
